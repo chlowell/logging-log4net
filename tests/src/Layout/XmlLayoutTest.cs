0ccced5f4ae53fcc5d84421170c3bcdd0575f142
Charles Lowell <chlowe@microsoft.com>
2017-07-25 15:22:17 -0700

    use obsolete XmlConvert.ToString to ensure strings match expectations

--- tests/src/Layout/XmlLayoutTest.cs
+++ tests/src/Layout/XmlLayoutTest.cs
@@ -83,7 +83,7 @@ namespace log4net.Tests.Layout
 		private static string CreateEventNode(string message)
 		{
 			return String.Format("<event logger=\"TestLogger\" timestamp=\"{0}\" level=\"INFO\" thread=\"TestThread\" domain=\"Tests\" identity=\"TestRunner\" username=\"TestRunner\"><message>{1}</message></event>" + Environment.NewLine,
-#if NET_2_0 || MONO_2_0 || MONO_3_5 || MONO_4_0 || NETCOREAPP1_0 || NETCOREAPP2_0
+#if NET_2_0 || MONO_2_0 || MONO_3_5 || MONO_4_0 || NETCOREAPP1_0
 			                     XmlConvert.ToString(DateTime.Today, XmlDateTimeSerializationMode.Local),
 #else
                                  XmlConvert.ToString(DateTime.Today),
@@ -94,7 +94,7 @@ namespace log4net.Tests.Layout
 		private static string CreateEventNode(string key, string value)
 		{
 			return String.Format("<event logger=\"TestLogger\" timestamp=\"{0}\" level=\"INFO\" thread=\"TestThread\" domain=\"Tests\" identity=\"TestRunner\" username=\"TestRunner\"><message>Test message</message><properties><data name=\"{1}\" value=\"{2}\" /></properties></event>" + Environment.NewLine,
-#if NET_2_0 || MONO_2_0 || MONO_3_5 || MONO_4_0 || NETCOREAPP1_0 || NETCOREAPP2_0
+#if NET_2_0 || MONO_2_0 || MONO_3_5 || MONO_4_0 || NETCOREAPP1_0
 			                     XmlConvert.ToString(DateTime.Today, XmlDateTimeSerializationMode.Local),
 #else
                                  XmlConvert.ToString(DateTime.Today),
